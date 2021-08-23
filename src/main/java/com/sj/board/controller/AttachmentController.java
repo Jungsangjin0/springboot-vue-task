95c4273e6a8d34d332f108f7e37b3ee97afaa2e3
loky13 <loky13@daum.net>
2021-08-23 13:37:40 +0900

    vue front folder 성생

--- src/main/java/com/sj/board/controller/AttachmentController.java
+++ src/main/java/com/sj/board/controller/AttachmentController.java
@@ -4,11 +4,18 @@ import com.sj.board.dto.AttachmentDto;
 import com.sj.board.file.FileStore;
 import com.sj.board.service.AttachmentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -43,11 +50,18 @@ public class AttachmentController {
      * @return
      */
     @GetMapping("/files/{fileId}")
-    public AttachmentDto downloadFile(@PathVariable long fileId) {
+    public ResponseEntity<Resource> downloadFile(@PathVariable long fileId) throws MalformedURLException {
 
         AttachmentDto file = attachmentService.findById(fileId);
 
-        return file;
+        UrlResource resource = new UrlResource("file:" + file.getFilePath());
+
+        String encodeUploadFilesName = UriUtils.encode(file.getOriginName(), StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodeUploadFilesName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(resource);
     }
 
     /**
